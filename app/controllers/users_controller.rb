efdea2eed355a4e0c724ab4b25d72b73b6066ecc
prajitlal4 <prajit.lal4@gmail.com>
2023-10-17 19:39:23 +0800

    Created function with standard HTML for rails form. defined private params for user object. now sending in hashed data from form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
  
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password]) #grabbed params from form
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password]) # grabbed params from form
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -12,6 +13,12 @@ class UsersController < ApplicationController
   
   def new
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
   end
 
 end
