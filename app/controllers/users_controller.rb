29aba48daf995ac45a2c1d29e474eb607cde9578
prajitlal4 <prajit.lal4@gmail.com>
2023-10-17 19:52:50 +0800

    created form using form_tag helper method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
  
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password]) # grabbed params from form
-    @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password]) # grabbed params from form
+    # @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
